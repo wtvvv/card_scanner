db58d89df5919de1ef692cc57bedbd7ba13d6d27
wtvvv <86994323+wtvvv@users.noreply.github.com>
2022-07-29 11:36:49 +0800

    Update scanformepls.py

--- scanformepls.py
+++ scanformepls.py
@@ -4,7 +4,7 @@ import time
 from threading import Thread
 from threading import Event
 
-FILENUMBER_BEGIN = 1673
+FILENUMBER_BEGIN = 1782
 FILENAME = "UGO00xxxx_common_12MP"
 FILEMENU_REGION = (5, 286, 320, 708) #set to None if unknown
 YES_OR_RENAME_REGION = (850, 450, 360, 160) #set to None if unknown
@@ -21,29 +21,31 @@ def scanCard(filenumber, exitEvent):
     exitEvent.wait(40)
     if exitEvent.is_set():
         exit()
-    #rotate
-    rotated = False
-    while not rotated:
+    #rename
+    doneScanning = False
+    while not doneScanning:
         # try:
         #     pyautogui.locateCenterOnScreen('renamebutton.png')
         # except ImageNotFoundException:
         #     pass
         time.sleep(2)
         if(not (pyautogui.locateCenterOnScreen('rotatebutton.png', region=ROTATE_REGION) is None)):
-            pyautogui.click(974, 57)
-            rotated = True
-    exitEvent.wait(10)
-    if exitEvent.is_set():
-        exit()
-    #rename
+            doneScanning = True
     x, y, width, height = pyautogui.locateOnScreen('newimagebox.png',  region=FILEMENU_REGION)
     pyautogui.rightClick(x, y)
     pyautogui.move(20, 20)
     pyautogui.click()
     pyautogui.write(FILENAME.replace('xxxx', str(filenumber)), interval = 0.1)
-    time.sleep(1)
+    time.sleep(0.2)
     x, y = pyautogui.locateCenterOnScreen('renamebutton.png', region=YES_OR_RENAME_REGION)
-    pyautogui.click(x, y)
+    pyautogui.click(x, y)    
+    time.sleep(1)
+
+    #rotate
+    pyautogui.click(974, 57)
+    exitEvent.wait(6)
+    if exitEvent.is_set():
+        exit()
 
     #alert
     pyautogui.alert(text='NEEEEEEEEEXXXXXTTTTTTTT CAAAAAARRRRRRRDDDDDDD PLLLEAAASEE', title='Scan finished', button='OK')
@@ -125,6 +127,7 @@ if __name__ == "__main__":
         
         if keyboard.is_pressed('ctrl+alt+r') and canScan and not canCancel:
             try:
+                pyautogui.confirm('Enter option Gfg', buttons =['A', 'B', 'C'])
                 filenumber = int(pyautogui.prompt(text='Enter starting file number', title='' , default=''))
             except:
                 pass
@@ -140,3 +143,4 @@ if __name__ == "__main__":
 
 #implement dynamic click for scan, rotate, and cancel!!!
 #implement clicking regulabutton before clicking anything else!!!
+#change ALL time.sleep(x) into event.wait(x)

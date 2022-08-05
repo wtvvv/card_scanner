058fafa7447b7610ec1fbbe50ac46f6eb081cfae
wtvvv <86994323+wtvvv@users.noreply.github.com>
2022-08-05 15:21:06 +0800

    added a batch file
    Modify the filepaths in the batch file, and create desktop shortcut for use

---
+++ scanformepls.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem path to activate conda env, make sure conda env has required packages
+cd "C:\Users\Scanning"
+call conda activate basee
+echo activated conda-base
+
+rem path to project directory
+cd desktop\scanformepls
+echo opened project directory
+
+rem python script file path
+python "C:\Users\Scanning\Desktop\scanformepls\scanformepls.py"
+pause
